78e450628dfc126838500223f21267404af70975
shubham.sc <shubham@skills.cafe>
2025-07-03 13:44:51 +0530

    email feature implimented

--- model/File.js
+++ model/File.js
@@ -1,10 +1,35 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
 
 const fileSchema = new mongoose.Schema({
   name: String,
   email: String,
   tag: String,
   imageUrl: String,
+});
+
+// Schema.post("save", ...) runs after a document is saved to the database.
+// It gives you access to the saved document.
+
+fileSchema.post("save", (doc) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.HOST_NAME,
+      auth: {
+        user: process.env.EMAILER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    transporter.sendMail({
+      from: "shubham's backend app",
+      to: doc.email,
+      subject: "media uploded succesfully",
+      html: `<p>media uploded succesfully at following link <a href="${doc.imageUrl}">${doc.imageUrl}</a></p>`,
+    });
+  } catch (e) {
+    console.log(e.message);
+  }
 });
 
 module.exports = mongoose.model("File", fileSchema);
